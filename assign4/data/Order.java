c631c78ad003081227de5b0231bcdbaa68960e70
Clinton Stovall <cstova01@uafs.edu>
2026-09-20 23:04:28 -0500

    Work from Sunday

--- assign4/data/Order.java
+++ assign4/data/Order.java
@@ -1,7 +1,29 @@
 package data;
 
 public class Order {
-
+	
+	private int orderID;
+	private String product;
+	private double totalAmount;
+	
+	public Order (int orderID, String product, double totalAmount) {
+		this.orderID = orderID;
+		this.product = product;
+		this.totalAmount = totalAmount;
+		
+	}
+	public int getOrderID() {
+		return orderID;
+	}
+	public String getProduct() {
+		return product;
+	}
+	public double getTotalAmount() {
+		return totalAmount;
+	}
+		
+	
+	
 	
 }
 
