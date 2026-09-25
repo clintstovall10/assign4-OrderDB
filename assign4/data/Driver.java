c985e619e5d1874afcaae18fa2b9b3a246a2c2d0
Clinton Stovall <cstova01@uafs.edu>
2026-09-24 20:49:43 -0500

    Final Work for Assign4

--- assign4/data/Driver.java
+++ assign4/data/Driver.java
@@ -5,11 +5,12 @@ public class Driver {
 
 	public static void main(String[] args) throws IOException {
 		
+		//create OrderDB object
 		OrderDB orders = new OrderDB();
+		//pass orders.txt into loadOrders
 		orders.loadOrders("orders.txt");
+		//display contents
 		orders.showOrders();
-		
-		
 		
 	}
 
--- assign4/data/OrderDB.java
+++ assign4/data/OrderDB.java
@@ -5,52 +5,70 @@ import java.io.IOException;
 
 
 public class OrderDB {
+	
 	private Order [] ordersArray;
+	
+	//counts the number of orders 
+	
+	private int orderNumbers = 0;
 	
 
 	
 	public void loadOrders(String orderstxt) throws IOException {
+		//add my readers
 		FileReader fr = new FileReader (orderstxt);
 		BufferedReader br = new BufferedReader (fr);
+		
+		//read the first line
 		String line = br.readLine();
+		
+		//declare and initialize my variables 
 		int orderID = 0;
 		String product = "";
 		double total = 0.00;
-		int index = 0;
+		
+		//read the next line because we don't want the header
 		line = br.readLine();
 		
+		//initialize the size of my array
 		ordersArray = new Order [100];
 		
 			while ( line != null) {
-		
+				//each line into its on array element by using comma as a delimiter
 				String [] split = line.split(",");
-			
+				
+				//I knew what elements I wanted to extract from each line of orders and converted
+				//string elements into either int or double
 				orderID = Integer.parseInt(split[0]);
 				product = split[2];
 				total = Double.parseDouble(split[3]);
 			
+				//called the order class and passed each variable into it, to store each orders values
 				Order order = new Order(orderID, product, total);
+
+				ordersArray[orderNumbers] = order;
+				orderNumbers++;
 			
-			
-				ordersArray[index] = order;
-				index++;
-			
-					
 				line = br.readLine();
 			
 			}
+			
 		br.close();
 	}
 	
 	public void showOrders() {
+		
 		int index;
-		for (index = 0; index < ordersArray.length; index++) {
+		System.out.printf("%-8s %-30s %10s%n", "Order ID", "Product", "Total Amt");
+		System.out.printf("%-8s %-30s %10s%n", "--------", "-------", "---------");
+		
+		//for loop to iterate through each line of orders and call the order class to get values at each line
+		for (index = 0; index < orderNumbers; index++) {
 			Order order = ordersArray[index];
 			order.getOrderID();
 			order.getProduct();
 			order.getTotalAmount();
-			System.out.println(order.getOrderID() + "," + order.getProduct() + "," + 
-			order.getTotalAmount());
+			System.out.printf("%-8d %-30s %10.2f%n", order.getOrderID(), order.getProduct(), order.getTotalAmount());
 		}
 		
 		
